cd34a377ecd0d844e90f5a0d7d05218bb5bc1624
agata.okolska <agata.okolska@gmail.com>
2018-12-30 16:04:18 +0100

    add socks in different color

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
 var app =  new Vue({
     el: '#app',
     data: {
-        product: 'socs',
+        product: 'socks',
         image: './assets/vmSocks-green-onWhite.jpg',
         inStock: true,
-        details: ['80% cotton', '20% polyester', 'male/female']
-
+        details: ['80% cotton', '20% polyester', 'male/female'],
+        variants: [
+            {
+                variantId: 111,
+                variantColor: 'green',
+                variantImage: './assets/vmSocks-green-onWhite.jpg',
+            },
+            {
+                variantId: 222,
+                variantColor: 'blue',
+                variantImage: './assets/vmSocks-blue-onWhite.jpg',
+            }
+        ],
+    cart: 0
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+        }
     }
 })
