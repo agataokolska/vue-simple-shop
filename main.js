ffed6b0120db3dc0eb265138d198de3c5496725a
agata.okolska <agata.okolska@gmail.com>
2018-12-30 16:56:43 +0100

    practice with using computed properties

--- main.js
+++ main.js
@@ -2,19 +2,21 @@ var app =  new Vue({
     el: '#app',
     data: {
         product: 'socks',
-        image: './assets/vmSocks-green-onWhite.jpg',
-        inStock: true,
+        brand: 'Vue special',
+        selectedVariant: 0,
         details: ['80% cotton', '20% polyester', 'male/female'],
         variants: [
             {
                 variantId: 111,
                 variantColor: 'green',
                 variantImage: './assets/vmSocks-green-onWhite.jpg',
+                variantQuantity: 11
             },
             {
                 variantId: 222,
                 variantColor: 'blue',
                 variantImage: './assets/vmSocks-blue-onWhite.jpg',
+                variantQuantity: 0
             }
         ],
     cart: 0
@@ -23,8 +25,19 @@ var app =  new Vue({
         addToCart() {
             this.cart += 1
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity
         }
     }
 })
