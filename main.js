03ab628518791415d67ddc134db4407887ec4671
agata.okolska <agata.okolska@gmail.com>
2018-12-30 18:11:50 +0100

    add form to submit reviews

--- main.js
+++ main.js
@@ -35,6 +35,19 @@ Vue.component('product', {
             </button>
             
         </div>
+        
+        <div>
+        <h2>Reviews</h2>
+        <p v-show="!reviews.length">There are no reviews yet</p>
+        <ul>
+        <li v-for="review in reviews">
+        <p>{{review.name}}</p>
+        <p>Rating: {{review.rating}}</p>
+        <p>{{review.review}}</p>
+</li>
+</ul>
+        </div>
+        <product-review @review-submitted="addReview"></product-review>
      </div>
     `,
     data() {
@@ -57,14 +70,18 @@ Vue.component('product', {
                     variantQuantity: 0
                 }
             ],
+            reviews: []
         }
     },
     methods: {
         addToCart() {
-           this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId)
+            this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId)
         },
         updateProduct(index) {
             this.selectedVariant = index
+        },
+        addReview(productReview) {
+            this.reviews.push(productReview)
         }
     },
     computed: {
@@ -83,7 +100,7 @@ Vue.component('product', {
             // }
         },
         shipping() {
-            if(this.premium) {
+            if (this.premium) {
                 return "Free"
             } else {
                 return "12 zł"
@@ -92,6 +109,59 @@ Vue.component('product', {
     }
 })
 
+
+Vue.component('product-review', {
+    template: `
+<form class="review-form" @submit.prevent="onSubmit">
+    <p>
+        <label for="name">Name:</label>
+        <input id="name" v-model="name" placeholder="name">
+    </p>
+
+    <p>
+        <label for="review">Review:</label>
+        <textarea id="review" v-model="review" placeholder="write your review here"></textarea>
+    </p>
+    
+    <p>
+        <label for="rating">Rating:</label>
+        <select id="rating" v-model.number="rating">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+        </select>
+    </p>
+    
+    <p>
+        <input type="submit" value="Submit">
+</p>
+    
+</form>
+    <input>
+    `,
+    data() {
+        return {
+            name: null,
+            review: null,
+            rating: null
+        }
+    },
+    methods: {
+        onSubmit() {
+            let productReview = {
+                name: this.name,
+                review: this.review,
+                rating: this.rating
+            }
+                this.$emit('review-submitted', productReview)
+                this.name = null,
+                this.review = null,
+                this.rating = null
+        }
+    }
+})
 
 var app = new Vue({
     el: '#app',
