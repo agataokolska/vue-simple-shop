626279f5cf38588d7b43df24bf59f0986a2f23dd
agata.okolska <agata.okolska@gmail.com>
2018-12-30 18:21:34 +0100

    add form validation and display errors

--- main.js
+++ main.js
@@ -134,9 +134,16 @@ Vue.component('product-review', {
         </select>
     </p>
     
+    <p v-if="errors.length">
+    <b>"Please correct the following error(s):</b>
+    <ul>
+        <li v-for="error in errors">{{ error }}</li>
+    </ul>
+    </p>
+    
     <p>
         <input type="submit" value="Submit">
-</p>
+    </p>
     
 </form>
     <input>
@@ -145,20 +152,27 @@ Vue.component('product-review', {
         return {
             name: null,
             review: null,
-            rating: null
+            rating: null,
+            errors: []
         }
     },
     methods: {
         onSubmit() {
-            let productReview = {
-                name: this.name,
-                review: this.review,
-                rating: this.rating
-            }
+            if (this.name && this.rating && this.review) {
+                let productReview = {
+                    name: this.name,
+                    review: this.review,
+                    rating: this.rating
+                }
                 this.$emit('review-submitted', productReview)
                 this.name = null,
-                this.review = null,
-                this.rating = null
+                    this.review = null,
+                    this.rating = null
+            } else {
+                if (!this.name) this.errors.push("Name required")
+                if (!this.review) this.errors.push("Review required")
+                if (!this.rating) this.errors.push("Rating required")
+            }
         }
     }
 })
